395aa3fcc51320e9c0b8ee1d37e9fed4ee3f0d68
Tomiwa Adelae <adelaetomiwa6@gmail.com>
2026-03-05 11:52:38 +0100

    Updated the UX

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -91,7 +91,7 @@ export function MobileNavbar() {
           </div>
         </ScrollArea>
         <SheetFooter className="mb-10">
-          <Button asChild>
+          <Button onClick={handleLinkClick} asChild>
             <Link href="/request-quote">Request Quote</Link>
           </Button>
         </SheetFooter>
